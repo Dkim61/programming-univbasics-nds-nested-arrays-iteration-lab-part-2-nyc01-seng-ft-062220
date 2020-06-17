671b5c40c3fa2ed21807df9047a33c5414d05c6e
david <dkim187@binghamton.edu>
2020-06-17 04:45:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_min_in_nested_arrays(src)
   lowest_temps = []
     while count < src.length do
         lowest_temps << src[count].min
-        count 0+=
+        count += 1
     end
-  count += 1
+  lowest_temps
 end
